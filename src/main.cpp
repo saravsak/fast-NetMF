1e88714192f7c84d8fc3d2624cc00219eb4f6c91
Saravanakumar <savkumar90@gmail.com>
2018-11-09 17:44:37 -0500

    Removed netmf for cuda

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,4 @@
 #include<iostream>
 
 int main(){
-	//Graph G = read_graph("../data/blogcatalog/edges.csv","edgelist");	
-	Graph G = read_graph("../data/test/test.csv","edgelist");	
-	//G.print_graph();
-	std::cout<<G.volume;
 }
--- src/make.sh
+++ src/make.sh
@@ -1,4 +1,4 @@
 nvcc -o graph.o -c graph.cpp
 nvcc -o utils.o -c utils.cpp
-nvcc main.cpp graph.o utils.o
-
+nvcc -o netmf.o -c netmf.cpp
+nvcc main.cpp graph.o utils.o netmf.o
