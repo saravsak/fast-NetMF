5a8561456f4979e88168d726546d7f674cf80000
Saravanakumar <savkumar90@gmail.com>
2018-11-15 21:19:46 -0500

    Need to add cuSolver stuff

--- gpu/main.cu
+++ gpu/main.cu
@@ -1,3 +1,16 @@
+/* 
+TODO: 
+1. Change all doubles to float 
+2. USe cuBlas for addition
+*/
+
+/* 
+Question for prof
+1. Copy stuff within GPU
+2. Launch kernel without copy
+3. Is it better to do more redundant work in one thread or one more kernle to do it once?
+4. Results are wrong if I use same variable as result. Why?
+*/
 # include<stdlib.h>
 # include<cuda_runtime.h>
 # include<cublas_v2.h>
@@ -18,6 +31,32 @@ __global__ void compute_d(double* deg, int size){
 	// Make assumption here that graph is connected and every node has degree atleast 1.
 	deg[id] = sqrt(1/deg[id]); 
 }
+
+__global__ void compute_s(double* S, double* X, int size){
+	int id = blockIdx.x * blockDim.x + threadIdx.x;
+
+	if(id >= size * size) return;
+	
+	S[id] += X[id]; 
+}
+
+__global__ void transform_s(double* S, int volume, int window_size, int b, int size){
+	int id = blockIdx.x * blockDim.x + threadIdx.x;
+
+	if(id >= size * size) return;
+	
+	S[id] = (S[id] * float(volume))/ ((float) window_size * (float) b); 
+}
+
+__global__ void transform_m(double* M, int size){
+	int id = blockIdx.x * blockDim.x + threadIdx.x;
+
+	if(id >= size * size) return;
+	
+	M[id] =logf(M[id] > 1?M[id]:1) 
+}
+
+
 
 int main ( void ){
 
@@ -41,26 +80,40 @@ int main ( void ){
 	/* Initialize and allocate variables */
 	// HOST
 	double *X;
-	X = (double *)malloc(g.size * g.size * sizeof(double));
+	double *S;
+	double *M;
+	
+	int window_size = 10;
+	int size = g.size * g.size * sizeof(double);
+	int b = 2;
+	
+	X = (double *)malloc(size);
+	S = (double *)malloc(size);
+	M = (double *)malloc(size);	
 
 	// DEVICE
 	double *D_device;
 	double *temp_device, *X_device;
 	double *A_device;
-	
-	cudaMalloc(&D_device, g.size * g.size * sizeof(double));
-	cudaMalloc(&A_device, g.size * g.size * sizeof(double));
-	cudaMalloc(&X_device, g.size * g.size * sizeof(double));
-	cudaMalloc(&temp_device, g.size * g.size * sizeof(double));
+	double *S_device;
+	double *M_device;
 
-	cudaMemset(A_device, 0, g.size * g.size * sizeof(double));
-	cudaMemset(D_device, 0, g.size * g.size * sizeof(double));
-	cudaMemset(X_device, 0, g.size * g.size * sizeof(double));
-	cudaMemset(temp_device, 0, g.size * g.size * sizeof(double));
+	cudaMalloc(&D_device, size);
+	cudaMalloc(&A_device, size);
+	cudaMalloc(&X_device, size);
+	cudaMalloc(&temp_device, size);
+	cudaMalloc(&S_device, size);
+	cudaMalloc(&M_device, size);
+
+	cudaMemset(A_device, 0, size);
+	cudaMemset(D_device, 0, size);
+	cudaMemset(X_device, 0, size);
+	cudaMemset(S_device, 0, size);
+	cudaMemset(M_device, 0, size);
 
 	/* Copy necessary variables to device */
-	cudaMemcpy(D_device, g.degree, g.size * g.size * sizeof(double), cudaMemcpyHostToDevice);	
-	cudaMemcpy(A_device, g.adj, g.size * g.size * sizeof(double), cudaMemcpyHostToDevice);	
+	cudaMemcpy(D_device, g.degree, size, cudaMemcpyHostToDevice);	
+	cudaMemcpy(A_device, g.adj, size , cudaMemcpyHostToDevice);	
 
 	/* Compute D = D^{-1/2} */
 	compute_d<<<grid, threads>>>(D_device, g.size);
@@ -87,61 +140,66 @@ int main ( void ){
 	            temp_device,g.size, 
 		    &bet, 
 		    X_device, g.size);
-
-	// Copy X to host
-	cudaMemcpy(X, X_device, g.size * g.size * sizeof(double), cudaMemcpyDeviceToHost);
 	
-	// Print X
-	std::cout<<std::endl;
-	std::cout<<std::endl;
-	std::cout<<"Printing X"<<std::endl;
+	/* Compute S = sum(X^{0}....X^{window_size}) */
+	
+	// Find alternate way to copy stuff within GPU
+	cudaMemcpy(X, X_device, size, cudaMemcpyDeviceToHost);
+	cudaMemcpy(S_device, X, size, cudaMemcpyHostToDevice);
+	cudaMemcpy(temp_device, X, size, cudaMemcpyHostToDevice);
+
+	for(int i=2;i<=window_size;i++){
+		cublasDgemm(handle, 
+		    CUBLAS_OP_N, CUBLAS_OP_N, 
+		    g.size, g.size, g.size,
+		    &al,
+		    temp_device, g.size,
+	            X_device,g.size, 
+		    &bet, 
+		    temp_device, g.size);
+
+		// USe cublas addition functions
+		compute_s<<<grid, threads>>>(S_device, temp_device, g.size);
+		cudaMemcpy(S, S_device, size, cudaMemcpyDeviceToHost);
+	}
+
+	// Compute S = S * (vol / (window_size * b))
+	transform_s<<<grid,threads>>>(S_device,g.volume, window_size, b, g.size);
+	cudaMemcpy(S, S_device, size, cudaMemcpyDeviceToHost);
+	
+	std::cout<<std::endl<<std::endl;
 	for(int i=0;i<g.size;i++){
 		for(int j=0;j<g.size;j++){
-			std::cout<<X[i*g.size + j]<<" ";
+			std::cout<<S[i*g.size + j]<<" ";
 		}
 		std::cout<<std::endl;
 	}
 	
-	/* Compute S = sum(X^{0}....X^{window_size}) */
-		
-	// Compute S = S * (vol / (window_size * b))
-	
 	// Compute M = D^{-1/2} * S * D^{-1/2}
-	
+	cudaMemset(temp_device, 0, size); 
+
+	cublasDgemm(handle, 
+	    CUBLAS_OP_N, CUBLAS_OP_N, 
+	    g.size, g.size, g.size,
+	    &al,
+	    S_device, g.size,
+	    D_device,g.size, 
+	    &bet, 
+	    temp_device, g.size);
+
+	cublasDgemm(handle, 
+	    CUBLAS_OP_N, CUBLAS_OP_N, 
+	    g.size, g.size, g.size,
+	    &al,
+	    D_device, g.size,
+	    temp_device,g.size, 
+	    &bet, 
+	    M_device, g.size);
+		
 	// Compute M = log(max(Mi,1))
-
-	/*****************
- 	*  CUBLAS SAMPLE *
-	*****************/
-
-//	cudaError_t cudaStat;
-//	cublasStatus_t stat;
-//	cublasHandle_t handle;
-//	int j;
-//	float *x;
-//	x = (float*) malloc(n*sizeof(*x));
-//	for(j=0;j<n;j++)
-//		x[j] = (float) j;
-//
-//	float *d_x;
-//
-//	cudaStat = cudaMalloc((void**)&d_x, n*sizeof(*x));
-//
-//	stat = cublasCreate(&handle);
-//
-//	stat = cublasSetVector(n, sizeof(*x), x, 1, d_x, 1);
-//	int result;
-//
-//	stat = cublasIsamax(handle, n, d_x, 1, &result);
-//
-//	std::cout<<x[result-1]<<std::endl;
-//
-//	cudaFree(d_x);
-//	cublasDestroy(handle);
-//	free(x);
-//	return EXIT_SUCCESS;
-//
-
+	transform_m<<<grid,threads>>>(M_device, g.size);
+	cudaMemcpy(M, M_device, size, cudaMemcpyDeviceToHost);
+	
 	/***********
 	* Clean up *
 	***********/
@@ -149,4 +207,15 @@ int main ( void ){
 	//free(g);
 
 	//cudaFree(D_device);
+
+	// Function to print matrix
+/*
+	std::cout<<std::endl<<std::endl;
+	for(int i=0;i<g.size;i++){
+		for(int j=0;j<g.size;j++){
+			std::cout<<S[i*g.size + j]<<" ";
+		}
+		std::cout<<std::endl;
+	}
+*/
 }
