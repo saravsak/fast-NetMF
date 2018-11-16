34d63b27ab06585333ff9427e6773cb72f46e215
Saravanakumar <savkumar90@gmail.com>
2018-11-15 20:12:40 -0500

    Code refactor - Reorganized sections

--- gpu/main.cu
+++ gpu/main.cu
@@ -1,8 +1,3 @@
-// Questionf for prof
-// 1. Do we need to store in column major order for cuBlas
-// 2. Do we need to copy everytime or once is enough
-// 3. Ask if DAD calculation is correct
-
 # include<stdlib.h>
 # include<cuda_runtime.h>
 # include<cublas_v2.h>
@@ -29,92 +24,69 @@ int main ( void ){
 	/**************
  	* NetMF small *
 	**************/
-
+	
 	/* Load graph */
 	Graph g =  read_graph("../data/test/small_test.csv","edgelist");
-	std::cout<<"Printing i/p graph"<<std::endl;
-	g.print_graph();
-	std::cout<<std::endl;
-	std::cout<<std::endl;
-	std::cout<<"Printing degree matrix"<<std::endl;
-	g.print_degree();
 
-
-	// Initialize all host and device variables
-	double *D_device;
-	double *temp_device, *X, *X_device;
-	double *A_device;
-	
-	X = (double *)malloc(g.size * g.size * sizeof(double));
-	
-	cudaMalloc(&D_device, g.size * g.size * sizeof(double));
-	cudaMalloc(&X_device, g.size * g.size * sizeof(double));
-	cudaMalloc(&A_device, g.size * g.size * sizeof(double));
-	cudaMalloc(&temp_device, g.size * g.size * sizeof(double));
-
-	cudaMemset(X_device, 0, g.size * g.size * sizeof(double));
-	cudaMemset(temp_device, 0, g.size * g.size * sizeof(double));
-
-	/* Compute D = D^{-1/2} */
-	// Create and allocate device variable to hold degree matrix
-
-	// Copy degree matrix to device
-	cudaMemcpy(D_device, g.degree, g.size * g.size * sizeof(double), cudaMemcpyHostToDevice);	
-	
-	// Call GPU Kernel
-
-	std::cout<<"number of grids: "<<(int)ceil((float)g.size/128)<<std::endl;	
-
+	/* CUDA housekeeping */
 	dim3 threads(128);
 	dim3 grid((int)ceil((float)g.size/128));
-	compute_d<<<grid, threads>>>(D_device, g.size);
 
-	// Copy from device to host
-	cudaMemcpy(g.degree, D_device, g.size * g.size * sizeof(double), cudaMemcpyDeviceToHost);
-
-	// Print results
-	std::cout<<std::endl;
-	std::cout<<std::endl;
-	std::cout<<"Printing D^{-1/2}"<<std::endl;
-	g.print_degree();
-
-	/* Compute X = D^{-1/2}AD^{-1/2} */
-	// Declare and allocate X matrix
-
-
-	// Copy A and D to device 
-	cudaMemcpy(D_device, g.degree, g.size * g.size * sizeof(double), cudaMemcpyHostToDevice);	
-	cudaMemcpy(A_device, g.adj, g.size * g.size * sizeof(double), cudaMemcpyHostToDevice);	
-	
-	// Compute product on GPU
+	/* cuBlas housekeeping */	
 	cublasHandle_t handle;
-
 	cublasCreate(&handle);
 	double al=1.0f;
 	double bet=1.0f;
-	int size = g.size;
 
+	/* Initialize and allocate variables */
+	// HOST
+	double *X;
+	X = (double *)malloc(g.size * g.size * sizeof(double));
+
+	// DEVICE
+	double *D_device;
+	double *temp_device, *X_device;
+	double *A_device;
+	
+	cudaMalloc(&D_device, g.size * g.size * sizeof(double));
+	cudaMalloc(&A_device, g.size * g.size * sizeof(double));
+	cudaMalloc(&X_device, g.size * g.size * sizeof(double));
+	cudaMalloc(&temp_device, g.size * g.size * sizeof(double));
+
+	cudaMemset(A_device, 0, g.size * g.size * sizeof(double));
+	cudaMemset(D_device, 0, g.size * g.size * sizeof(double));
+	cudaMemset(X_device, 0, g.size * g.size * sizeof(double));
+	cudaMemset(temp_device, 0, g.size * g.size * sizeof(double));
+
+	/* Copy necessary variables to device */
+	cudaMemcpy(D_device, g.degree, g.size * g.size * sizeof(double), cudaMemcpyHostToDevice);	
+	cudaMemcpy(A_device, g.adj, g.size * g.size * sizeof(double), cudaMemcpyHostToDevice);	
+
+	/* Compute D = D^{-1/2} */
+	compute_d<<<grid, threads>>>(D_device, g.size);
+
+	/* Compute X = D^{-1/2}AD^{-1/2} */
 	/* NOTE: cuBlas takes matrices in col major order */
 	/* So instead of X = D^{-1/2}AD^{-1/2} we do      */
 	/* X = AD^{-1/2} -> X = X             */
 
 	cublasDgemm(handle, 
 		    CUBLAS_OP_N, CUBLAS_OP_N, 
-		    size, size, size,
+		    g.size, g.size, g.size,
 		    &al,
-	            A_device,size, 
-		    D_device, size,
+	            A_device,g.size, 
+		    D_device, g.size,
 		    &bet, 
-		    temp_device, size);
+		    temp_device, g.size);
 	
 	cublasDgemm(handle, 
 		    CUBLAS_OP_N, CUBLAS_OP_N, 
-		    size, size, size,
+		    g.size, g.size, g.size,
 		    &al,
-		    D_device, size,
-	            temp_device,size, 
+		    D_device, g.size,
+	            temp_device,g.size, 
 		    &bet, 
-		    X_device, size);
+		    X_device, g.size);
 
 	// Copy X to host
 	cudaMemcpy(X, X_device, g.size * g.size * sizeof(double), cudaMemcpyDeviceToHost);
