008ddf6856fee22802721ec00973412bd03ae1a4
Saravanakumar <savkumar90@gmail.com>
2018-12-01 21:04:39 -0500

    Almost computed deepwalk matrix correctly

--- gpu/make_large.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-nvcc -o ../utils/graph.o -c ../utils/graph.cpp
-nvcc -o ../utils/io.o -c ../utils/io.cpp
-nvcc -o ../utils/utils.o -c ../utils/utils.cpp
-nvcc -o netmf_large netmf_large.cu ../utils/graph.o ../utils/io.o ../utils/utils.o -lcublas -lcusolver
