d612d9da9d8caf5287031dc384919e67e5f01a14
Saravanakumar <savkumar90@gmail.com>
2018-12-13 18:44:34 -0500

    Added functions to graphio header file

--- utils/graphio.h
+++ utils/graphio.h
@@ -8,3 +8,8 @@ Graph read_graph(std::string, std::string);
 Graph read_graph_from_mat(std::string);
 Graph read_graph_from_metis(std::string);
 Graph read_graph_from_edgelist(std::string);
+
+SpGraph read_sparse_graph(std::string, std::string);
+SpGraph read_sparse_graph_from_mat(std::string);
+SpGraph read_sparse_graph_from_metis(std::string);
+SpGraph read_sparse_graph_from_edgelist(std::string);
