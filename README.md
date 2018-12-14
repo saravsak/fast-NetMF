eccc2e496a16d72adf5ed5b9ada7fb9f5828949c
Saravanakumar <savkumar90@gmail.com>
2018-12-13 19:18:47 -0500

    Sparse graph reading compiles without error

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # fast-NetMF
 A fast implementation of NetMF for multi-core CPUs and GPUs
+
+cd gpu
+./exec small for NetMF Small
+./exec large for NetMF large
+
+These will both run on the blogcatalog dataset and produce two files:
+
+blogcatalog.emb: The embedding file in gensim format
+profile.txt: Performance report
+
--- utils/io.h
+++ utils/io.h
@@ -5,9 +5,9 @@
 
 #include<string>
 
-Graph read_graph(std::string, std::string);
-Graph read_graph_from_mat(std::string);
-Graph read_graph_from_metis(std::string);
-Graph read_graph_from_edgelist(std::string);
+//Graph read_graph(std::string, std::string);
+//Graph read_graph_from_mat(std::string);
+//Graph read_graph_from_metis(std::string);
+//Graph read_graph_from_edgelist(std::string);
 void write_embeddings(const char *, float *embeddings, int size, int dim);
 void write_profile(const char *, info profile);
