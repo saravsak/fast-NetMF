5258a9c32c00ba2d285af6ae74cc99c9de53a53b
SK <shanmugamsakthivadivel.1@osu.edu>
2019-03-22 04:30:22 -0400

    Completed dense mkl. Tiling left

--- gpu/netmf_small_tiled_hybrid.cu
+++ gpu/netmf_small_tiled_hybrid.cu
@@ -20,6 +20,160 @@
 
 #include "omp.h"
 
+#define NUM_STREAMS 4
+#define TILE_SIZE 2
+
+#define gpuErrchk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
+
+inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=true)
+{
+   if (code != cudaSuccess)
+   {
+      fprintf(stderr,"GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
+      if (abort) exit(code);
+   }
+}
+
+void make_tile(DT* A, DT *B, int om, int on, int m, int n, int row_offset, int col_offset){
+        int row = 0, col = 0;
+        row_offset = row_offset * m;
+        col_offset = col_offset * n;
+
+	#pragma omp parallel
+	{
+		#pragma for collapse(2)
+        	for(int i=0;i<m;i++){
+                	row = row_offset + i;
+                	for(int j=0;j<n;j++){
+                        	col = col_offset + j;
+                        	B[i + j * m] = A[row + col * om];
+                	}
+        	}
+	}	
+}
+
+void copy_tile(DT* A, DT *B, int om, int on, int m, int n, int row_offset, int col_offset){
+
+        int row = 0, col = 0;
+        row_offset = row_offset * m;
+        col_offset = col_offset * n;
+
+	#pragma omp parallel
+	{
+		#pragma for collapse(2)
+        	for(int i=0;i<m;i++){
+        	        row = row_offset + i;
+        	        for(int j=0;j<n;j++){
+        	                col = col_offset + j;
+        	                A[row + col * om] = B[i + j * m];
+        	        }
+        	}
+	}
+}
+
+void tiled_dgmm(DT *diag, DT *A, DT *C, int m, int n){
+
+	cudaStream_t streams[NUM_STREAMS];
+
+	for(int i=0; i<NUM_STREAMS;i++){
+		cudaStreamCreate(&streams[i]);
+	}
+
+	DT *tile_d[NUM_STREAMS];
+	DT *tile_h[NUM_STREAMS];
+	
+	DT *diag_d[NUM_STREAMS];
+	DT *diag_h[NUM_STREAMS];
+
+	DT *res_d[NUM_STREAMS];
+	DT *res_h[NUM_STREAMS];
+	
+	int num_tiles = ceil(((float) m)/(TILE_SIZE));
+
+	log("Check 1");
+
+        for(int i=0;i<NUM_STREAMS;i++){
+                gpuErrchk(cudaMalloc(&tile_d[i],
+                                        TILE_SIZE * TILE_SIZE * sizeof(DT)));
+                gpuErrchk(cudaMalloc(&res_d[i],
+                                        TILE_SIZE * TILE_SIZE * sizeof(DT)));
+                gpuErrchk(cudaMalloc(&diag_d[i],
+                                        TILE_SIZE * sizeof(DT)));
+
+		gpuErrchk(cudaHostAlloc(&tile_h[i],
+                                TILE_SIZE*TILE_SIZE*sizeof(DT),
+                                cudaHostAllocDefault));
+		gpuErrchk(cudaHostAlloc(&res_h[i],
+                                TILE_SIZE*TILE_SIZE*sizeof(DT),
+                                cudaHostAllocDefault));
+		gpuErrchk(cudaHostAlloc(&diag_h[i],
+                                TILE_SIZE*sizeof(DT),
+                                cudaHostAllocDefault));
+        }
+	
+	log("Check 2");
+
+	cublasHandle_t cublasH;	
+
+	int row=0,col=0;
+
+	while(row < num_tiles){
+		col = 0;
+		while(col < num_tiles){
+			int ret_col = col;
+			for(int i=0;i<NUM_STREAMS;i++){
+				if(col > num_tiles){
+					break;
+				}
+				std::cout<<"Working on block: "<<row * col<<std::endl;
+				make_tile(A, tile_h[(row * col) % NUM_STREAMS], m, n, TILE_SIZE, TILE_SIZE, row, col);
+				log("Check 3");
+					
+				gpuErrchk(cudaMemcpyAsync(tile_d[(row * col) % NUM_STREAMS],
+							tile_h[(row * col) % NUM_STREAMS],
+							TILE_SIZE * TILE_SIZE * sizeof(DT),
+							cudaMemcpyHostToDevice,
+							streams[i]));
+				gpuErrchk(cudaMemcpyAsync(diag_d[row % NUM_STREAMS],
+							diag_h + (row * TILE_SIZE),
+							TILE_SIZE * sizeof(DT),
+							cudaMemcpyHostToDevice,
+							streams[i]));
+				log("Check 4");
+				cublasSetStream(cublasH, streams[i]);
+				
+				log("Check 5");
+				cublasSdgmm(cublasH, CUBLAS_SIDE_LEFT, m, n,
+					tile_d[(row*col) % NUM_STREAMS], m,
+					diag_d[row%NUM_STREAMS],1,
+					res_d[(row*col) % NUM_STREAMS],m);
+				log("Check 6");
+				col++;
+			}
+
+			for(int i=0;i<NUM_STREAMS;i++){
+				if(ret_col > num_tiles){
+					break;
+				}
+				cudaStreamSynchronize(streams[i]);
+
+				gpuErrchk(cudaMemcpyAsync(res_h[(row * ret_col) % NUM_STREAMS],
+							res_d[(row * ret_col) % NUM_STREAMS],
+							TILE_SIZE * TILE_SIZE * sizeof(DT),
+							cudaMemcpyHostToDevice,
+							streams[i]));
+				copy_tile(C, res_h[(row*ret_col) % NUM_STREAMS],m,n, TILE_SIZE, TILE_SIZE, row, ret_col);
+				ret_col++;
+
+			}	
+		}
+		row++;
+	}
+
+
+}
+
+
 int main(int argc, char *argv[]){
 	/********************
         * NetMF small tiled *
@@ -56,7 +210,7 @@ int main(int argc, char *argv[]){
 		#pragma omp for
 		for(int i=0;i<g.size;i++){
 			if(g.degree1D[i] == 0){
-				g.degree[i * g.size + i] = 1.00;
+				g.degree1D[i * g.size + i] = 1.00;
 				g.adj[i * g.size + i] = 1.00;
 			}else{
 				g.adj[i * g.size + i] = 1.00;
@@ -71,20 +225,15 @@ int main(int argc, char *argv[]){
 	{
 		#pragma omp for
 		for(int i=0;i<g.size;i++){
-			for(int j=0;j<g.size;j++){
-				g.degree[i * g.size + j] = 1.00 / sqrt(g.degree[i * g.size + j]);
-			}
+			g.degree1D[i] = 1.00 / sqrt(g.degree1D[i]);
 		}
 	}
 
 	/* Compute X = D' * A * D' */
 	log("Computing X' = D' * A");
-	DT *X_ = (DT *) mkl_malloc( g.size * g.size * sizeof(DT *), 64);
-	cblas_sgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans,
-			g.size, g.size, g.size,
-			1.00, g.degree, g.size,
-			g.adj, g.size,
-			0.00, X_, g.size );		
+	DT *X_ = (DT *) malloc(g.size * g.size * sizeof(DT));
+
+	
 
 	/* Compute S = sum(X^{0}....X^{window_size}) */
 
